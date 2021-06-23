5b0329a9b0ce00546add4953277e91afc4a99708
VSainteuf <vivien.sainte-fare-garnot@polytechnique.edu>
2021-06-23 17:52:08 +0200

    wip
    ignore
    
    wip
    
    dataloader doc
    
    docstring
    
    wip
    
    wip
    
    wip
    
    predictions
    
    doc

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# pastis-benchmark
+# PAnoptic Segmentation of satellite image TIme Series - PASTIS Benchmark
+
+![](images/predictions.png)
+
+## Dataset presentation
+PASTIS is a benchmark dataset for panoptic and semantic segmentation of agricultural parcels from 
+satellite image time series. It is composed of 2433 one square kilometer-patches in the French metropolitan territory for which sequences 
+of satellite observations are assembled into a four-dimensional spatio-temporal tensor. 
+The dataset contains both semantic and instance annotations, assigning to each pixel a semantic label and an instance id.
+There is an official 5 fold split provided in the dataset's metadata.
+## Usage 
+The dataset can be downloaded from [zenodo](https://zenodo.org/record/5012942).
+This repository also contains a PyTorch dataset class in `dataloader.py` that can be readily used to load data for training.
+
+## Leaderboard
+Please open an issue to submit new entries.
+
+### Semantic Segmentation
+| Model name         | #Params| OA  |  mIoU | Published |
+| ------------------ |---- |---- | ---| --- |
+| U-TAE   |   1.1M|  83.2%    | 63.1%|  :heavy_check_mark:|
+| Unet-3d*   | 1.6M|    81.3%    |  58.4%| :heavy_check_mark:|
+| Unet-ConvLSTM* |1.5M  |     82.1%    |  57.8%| :heavy_check_mark:|
+| FPN-ConvLSTM*  | 1.3M|    81.6%   |  57.1%|:heavy_check_mark: |
+
+Models that we re-implemented are denoted with a star (*).
+
+
+### Panoptic Segmentation
+| Model name         | SQ  | RQ | PQ|
+| ------------------ |--- | --- |--- |
+| U-TAE + PaPs       | 82.0|51.0 |42.2   |
+
+
+
+## Documentation
+The agricultural parcels are grouped into 18 different crop classes as shown in the 
+table below. 
+<img src="images/Nomenclature.jp2" alt="drawing" width="300"/>
+
+Additional information about the dataset can be found in the `doc.pdf` document.
+
+## References
+If you use PASTIS please cite the related paper:
+```
+@article{garnot2021panoptic,
+  title={Panoptic Segmentation of Satellite Image Time Series
+with Convolutional Temporal Attention Networks},
+  author={Sainte Fare Garnot, Vivien  and Landrieu, Loic },
+  journal={arxiv},
+  year={2021}
+}
+```
+
+## Credits
+
+- The satellite imagery used in PASTIS was retrieved from [THEIA](www.theia.land.fr): 
+"Value-added data processed by the CNES for the Theia www.theia.land.fr data cluster using Copernicus data.
+The treatments use algorithms developed by Theia’s Scientific Expertise Centres. "
+
+- The annotations used in PASTIS stem from the French [land parcel identification system](https://www.data.gouv.fr/en/datasets/registre-parcellaire-graphique-rpg-contours-des-parcelles-et-ilots-culturaux-et-leur-groupe-de-cultures-majoritaire/) produced
+ by IGN, the French mapping agency.
+ 
+- This work was partly supported by [ASP](https://www.asp-public.fr), the French Payment Agency. 
---
+++ dataloader.py
@@ -0,0 +1,254 @@
+import torch
+import torch.utils.data as tdata
+from torch.nn import functional as F
+
+import numpy as np
+import geopandas as gpd
+import pandas as pd
+
+from datetime import datetime
+import os
+import json
+import collections.abc
+import re
+
+
+class PASTIS_Dataset(tdata.Dataset):
+    def __init__(self, folder, norm=True, target='semantic', cache=False, mem16=False, folds=None,
+                 reference_date='2018-09-01', class_mapping=None, mono_date=None, sats=['S2']):
+        """
+        Pytorch Dataset class to load samples from the PASTIS dataset, for semantic and panoptic segmentation.
+        The Dataset yields ((data, dates), target) tuples, where:
+            - data contains the image time series
+            - dates contains the date sequence of the observations expressed in number of days since a reference date
+            - target is the semantic or instance target
+        Args:
+            folder (str): Path to the dataset
+            norm (bool): If true, images are standardised using pre-computed channel-wise
+            means and standard deviations.
+            reference_date (str, Format : 'YYYY-MM-DD'): Defines the reference date based on which all observation dates
+            are expressed. Along with the image time series and the target tensor, this dataloader yields the sequence
+            of observation dates (in terms of number of days since the reference date). This sequence of dates is used
+            for instance for the positional encoding in attention based approaches.
+            target (str): 'semantic' or 'instance'. Defines which type of target is returned by the dataloader.
+            If 'semantic' the target tensor is a tensor containing the class of each pixel.
+            If 'instance' the target tensor is the concatenation of several signals, necessary to train the
+            Parcel-as-Points module:
+                - the centerness heatmap,
+                - the instance ids,
+                - the voronoi partitioning of the patch with regards to the parcels' centers,
+                - the (height, width) size of each parcel
+                - the semantic label of each parcel
+                - the semantic label of each pixel
+            cache (bool): If True, the loaded samples stay in RAM, default False.
+            mem16 (bool): Additional argument for cache. If True, the image time series tensors are stored
+            in half precision in RAM for efficiency. They are cast back to float32 when returned by __getitem__.
+            folds (list, optional): List of ints specifying which of the 5 official folds to load.
+            By default (when None is specified) all folds are loaded.
+            class_mapping (dict, optional): Dictionary to define a mapping between the defaukt 18 class nomenclature
+            and another class grouping, optional.
+            mono_date (int or str, optional): If provided only one date of the available time series is loaded.
+            If argument is an int it defines the position of the date that is loaded. If it is a string, it should be
+            in format 'YYYY-MM-DD' and the closest available date will be selected.
+            sats (list): defines the satellites to use (only Sentinel-2 is available in v1.0)
+        """
+        super(PASTIS_Dataset, self).__init__()
+        self.folder = folder
+        self.norm = norm
+        self.reference_date = datetime(*map(int, reference_date.split('-')))
+        self.cache = cache
+        self.mem16 = mem16
+        self.mono_date = datetime(*map(int, mono_date.split('-'))) if isinstance(mono_date, str) else mono_date
+        self.memory = {}
+        self.memory_dates = {}
+        self.class_mapping = np.vectorize(lambda x: class_mapping[x]) if class_mapping is not None else class_mapping
+        self.target = target
+        self.sats = sats
+
+        # Get metadata
+        print('Reading patch metadata . . .')
+        self.meta_patch = gpd.read_file(os.path.join(folder, 'metadata.geojson'))
+        self.meta_patch.index = self.meta_patch['ID_PATCH'].astype(int)
+        self.meta_patch.sort_index(inplace=True)
+
+        self.date_tables = {s: None for s in sats}
+        self.date_range = np.array(range(-200, 600))
+        for s in sats:
+            dates = self.meta_patch['dates-{}'.format(s)]
+            date_table = pd.DataFrame(index=self.meta_patch.index, columns=self.date_range, dtype=int)
+            for pid, date_seq in dates.iteritems():
+                d = pd.DataFrame().from_dict(date_seq, orient='index')
+                d = d[0].apply(
+                    lambda x: (datetime(int(str(x)[:4]), int(str(x)[4:6]), int(str(x)[6:])) - self.reference_date).days)
+                date_table.loc[pid, d.values] = 1
+            date_table = date_table.fillna(0)
+            self.date_tables[s] = {index: np.array(list(d.values())) for index, d in
+                                   date_table.to_dict(orient='index').items()}
+
+        print('Done.')
+
+        # Select Fold samples
+        if folds is not None:
+            self.meta_patch = pd.concat([self.meta_patch[self.meta_patch['Fold'] == f] for f in folds])
+
+        self.len = self.meta_patch.shape[0]
+        self.id_patches = self.meta_patch.index
+
+        # Get normalisation values
+        if norm:
+            self.norm = {}
+            for s in self.sats:
+                with open(os.path.join(folder, 'NORM_{}_patch.json'.format(s)), 'r') as file:
+                    normvals = json.loads(file.read())
+                selected_folds = folds if folds is not None else range(1, 6)
+                means = [normvals['Fold_{}'.format(f)]['mean'] for f in selected_folds]
+                stds = [normvals['Fold_{}'.format(f)]['std'] for f in selected_folds]
+                self.norm[s] = np.stack(means).mean(axis=0), np.stack(stds).mean(axis=0)
+                self.norm[s] = torch.from_numpy(self.norm[s][0]).float(), torch.from_numpy(self.norm[s][1]).float()
+        else:
+            self.norm = None
+        print('Dataset ready.')
+
+    def __len__(self):
+        return self.len
+
+    def get_dates(self, id_patch, sat):
+        return self.date_range[np.where(self.date_tables[sat][id_patch] == 1)[0]]
+
+    def __getitem__(self, item):
+        id_patch = self.id_patches[item]
+
+        # Retrieve and prepare satellite data
+        if not self.cache or item not in self.memory.keys():
+            data = {satellite: np.load(
+                os.path.join(self.folder, 'DATA_{}'.format(satellite),
+                             '{}_{}.npy'.format(satellite, id_patch))).astype(np.float32)
+                    for satellite in self.sats}  # T x C x H x W arrays
+            data = {s: torch.from_numpy(a) for s, a in data.items()}
+            if self.norm is not None:
+                data = {s: (d - self.norm[s][0][None, :, None, None]) / self.norm[s][1][None, :, None, None] for s, d in
+                        data.items()}
+
+            if self.target == 'semantic':
+                target = np.load(os.path.join(self.folder, 'ANNOTATIONS', 'TARGET_{}.npy'.format(id_patch)))
+                target = torch.from_numpy(target[0].astype(int))
+                if self.class_mapping is not None:
+                    target = self.class_mapping(target)
+            elif self.target == 'instance':
+                heat = np.load(os.path.join(self.folder, 'INSTANCE_ANNOTATIONS', 'HEATMAP_{}.npy'.format(id_patch)))
+
+                inst = np.load(os.path.join(self.folder, 'INSTANCE_ANNOTATIONS', 'INSTANCES_{}.npy'.format(id_patch)))
+                zone = np.load(os.path.join(self.folder, 'INSTANCE_ANNOTATIONS', 'ZONES_{}.npy'.format(id_patch)))
+
+                sem_pix = np.load(os.path.join(self.folder, 'ANNOTATIONS', 'TARGET_{}.npy'.format(id_patch)))
+                if self.class_mapping is not None:
+                    sem_pix = self.class_mapping(sem_pix[0])
+                else:
+                    sem_pix = sem_pix[0]
+
+                size = np.zeros((*inst.shape, 2))
+                sem_obj = np.zeros(inst.shape)
+                for x in np.unique(inst):
+                    if x != 0:
+                        h = (inst == x).any(axis=-1).sum()
+                        w = (inst == x).any(axis=-2).sum()
+                        size[zone == x] = (h, w)
+                        sem_obj[zone == x] = sem_pix[inst == x][0]
+
+                target = torch.from_numpy(
+                    np.concatenate([heat[:, :, None], inst[:, :, None], zone[:, :, None], size,
+                                    sem_obj[:, :, None], sem_pix[:, :, None]], axis=-1)).float()
+
+            if self.cache:
+                if self.mem16:
+                    self.memory[item] = [{k: v.half() for k, v in data.items()}, target]
+                else:
+                    self.memory[item] = [data, target]
+
+        else:
+            data, target = self.memory[item]
+            if self.mem16:
+                data = {k: v.float() for k, v in data.items()}
+
+        # Retrieve date sequences
+        if not self.cache or id_patch not in self.memory_dates.keys():
+            dates = {s: torch.from_numpy(self.get_dates(id_patch, s)) for s in self.sats}
+            if self.cache:
+                self.memory_dates[id_patch] = dates
+        else:
+            dates = self.memory_dates[id_patch]
+
+        if self.mono_date is not None:
+            if isinstance(self.mono_date, int):
+                data = data[self.mono_date]
+                dates = dates[self.mono_date]
+            else:
+                mono_delta = (self.mono_date - self.reference_date).days
+                mono_date = (dates - mono_delta).abs().argmin()
+                data = data[mono_date]
+                dates = dates[mono_date]
+
+        return (data, dates), target
+
+
+def prepare_dates(date_dict, reference_date):
+    """Date formating."""
+    d = pd.DataFrame().from_dict(date_dict, orient='index')
+    d = d[0].apply(lambda x: (datetime(int(str(x)[:4]), int(str(x)[4:6]), int(str(x)[6:])) - reference_date).days)
+    return d.values
+
+
+np_str_obj_array_pattern = re.compile(r'[SaUO]')
+
+
+def pad_tensor(x, l, pad_value=0):
+    padlen = l - x.shape[0]
+    pad = [0 for _ in range(2 * len(x.shape[1:]))] + [0, padlen]
+    return F.pad(x, pad=pad, value=pad_value)
+
+
+def pad_collate(batch, pad_value=0):
+    # Utility function to be used as collate_fn for the PyTorch dataloader to handle sequences of varying length.
+    # Sequences are padded with zeros by default.
+    # modified default_collate from the official pytorch repo
+    # https://github.com/pytorch/pytorch/blob/master/torch/utils/data/_utils/collate.py
+    elem = batch[0]
+    elem_type = type(elem)
+    if isinstance(elem, torch.Tensor):
+        out = None
+        if len(elem.shape) > 0:
+            sizes = [e.shape[0] for e in batch]
+            m = max(sizes)
+            if not all(s == m for s in sizes):
+                # pad tensors which have a temporal dimension
+                batch = [pad_tensor(e, m, pad_value=pad_value) for e in batch]
+        if torch.utils.data.get_worker_info() is not None:
+            # If we're in a background process, concatenate directly into a
+            # shared memory tensor to avoid an extra copy
+            numel = sum([x.numel() for x in batch])
+            storage = elem.storage()._new_shared(numel)
+            out = elem.new(storage)
+        return torch.stack(batch, 0, out=out)
+    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
+            and elem_type.__name__ != 'string_':
+        if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
+            # array of string classes and object
+            if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
+                raise TypeError('Format not managed : {}'.format(elem.dtype))
+
+            return pad_collate([torch.as_tensor(b) for b in batch])
+        elif elem.shape == ():  # scalars
+            return torch.as_tensor(batch)
+    elif isinstance(elem, collections.abc.Mapping):
+        return {key: pad_collate([d[key] for d in batch]) for key in elem}
+    elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
+        return elem_type(*(pad_collate(samples) for samples in zip(*batch)))
+    elif isinstance(elem, collections.abc.Sequence):
+        # check to make sure that the elements in batch have consistent size
+        it = iter(batch)
+        elem_size = len(next(it))
+        if not all(len(elem) == elem_size for elem in it):
+            raise RuntimeError('each element in list of batch should be of equal size')
+        transposed = zip(*batch)
+        return [pad_collate(samples) for samples in transposed]
+    raise TypeError('Format not managed : {}'.format(elem_type))
